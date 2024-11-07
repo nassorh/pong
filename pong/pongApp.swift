180fe6ad405bd80920f3cd3eb9971e18c2d838aa
nassorh <nassorh.dev@gmail.com>
2024-11-07 10:27:37 +0000

    Initial Commit

---
+++ pong/pongApp.swift
@@ -0,0 +1,17 @@
+//
+//  pongApp.swift
+//  pong
+//
+//  Created by Nassor, Hamad on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct pongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
